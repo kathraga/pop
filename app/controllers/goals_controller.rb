84c9d6e4d640201ff118128390d8e34db4200fac
kathraga <ocmervans@msn.com>
2016-01-19 12:04:48 +0100

    goals added to competence view

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -1,2 +1,23 @@
 class GoalsController < ApplicationController
+
+  def create
+    @competence = Competence.find(params[:competence_id])
+    @goal = @competence.goals.create(goal_params)
+    redirect_to competence_path(@competence)
+  end
+
+   def destroy
+    @competence = Competence.find(params[:competence_id])
+    @goal = @competence.goals.find(params[:id])
+    @goal.destroy
+    redirect_to competence_path(@competence)
+  end
+
+  def new
+  end
+
+ private
+    def goal_params
+      params.require(:goal).permit(:description)
+    end
 end
